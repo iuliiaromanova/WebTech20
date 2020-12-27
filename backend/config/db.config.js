1c14f150968c121333d9c3db889631f74d7a8ef8
iuliia romanova <romanovajb@gmail.com>
2020-12-27 13:35:36 +0100

    Aufgabe 8 Node.js + Express (REST-Server + MySQL)

---
+++ backend/config/db.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    HOST: "localhost",
+    USER: "TestUser",
+    PASSWORD: "123qqq",
+    DB: "mockupdb"
+};
--- first/src/app/app.module.ts
+++ first/src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatSliderModule } from '@angular/material/slider';
+import { MycomponentComponent } from './mycomponent/mycomponent.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MycomponentComponent
   ],
   imports: [
     BrowserModule,
